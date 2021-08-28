a2cdc65b079860f4e15a1653c55f3331812bf753
shubhi704 <82434808+shubhi704@users.noreply.github.com>
2021-08-28 17:21:00 +0530

    Update ram.v

--- ram.v
+++ ram.v
@@ -1,1 +1,23 @@
+`timescale 1ns/1ns
+module ram(input clk,rst,we,
+           input [7:0]addr,
+           input [7:0] dataIn,
+           output reg[7:0] dataOut );
+integer i;
+  
+ reg [7:0]mem [0:7];
 
+           always @(posedge clk) begin
+            if(rst == 1'b1) begin
+              for(i=0;i<256; i++) begin
+                 mem[addr] <= 8'd0;
+                 end
+                 end
+            else if(rst == 1'b0) begin
+             if(we)
+              mem[addr] <= dataIn;
+             else
+              dataOut <= mem[addr];
+            end
+            end
+        endmodule 
